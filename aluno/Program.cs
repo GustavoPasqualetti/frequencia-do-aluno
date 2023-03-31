5da4b814f8a5da5c1acadf8a153e614be7a76563
Gustavo Pasqualetti <49802004863@info.local>
2023-03-31 08:45:39 -0300

    projeto frequencia e media do aluno

---
+++ aluno/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine($"Qual é a frequência do aluno");
+float frequencia= float.Parse(Console.ReadLine());
+
+Console.WriteLine($"Qual e a media do aluno");
+float media= float.Parse(Console.ReadLine());
+
+
+if (frequencia < 75 ) 
+{
+Console.WriteLine($"voce esta reprovado");
+
+}
+
+else if ( media >= 7 )
+{
+Console.WriteLine($"voce esta aprovado");
+
+}
+else if (media < 3)
+{
+ Console.WriteLine($"voce esta reprovado");
+ 
+}
+else 
+{
+Console.WriteLine($"voce tem direito a uma prova de recuperacao");
+
+}
+
